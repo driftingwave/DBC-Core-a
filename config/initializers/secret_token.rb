1a0bee452386764355d6af55facf35a97069bd4a
Adigo Atabo <Adigo@Adigos-MacBook-Air.local>
2013-10-30 12:50:18 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quora::Application.config.secret_token = 'c982ad6b4ea9412d9e5f33d257ef25ae75df72bc93f932dc581d76ef06e5af3d579d8ff1a183f534d5f2a4b174a30d626c51dc2093a3bda0422b86736fefea05'
