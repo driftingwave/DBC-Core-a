07299dbd3983bd1bc4a8f93db2fc88b809faf844
Apprentice <apprentice@dbc-chicago-001.local>
2013-10-30 17:21:08 -0500

    adding usersessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  # helper_method :current_user
 end
--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,3 +1,25 @@
 class UserSessionsController < ApplicationController
+
+  def new
+    @user_session = UserSession.new
+  end
+
+  def create 
+    if @user && @user.password_digest == User.password_digest(params[:password])
+      session[:logged_in] = true
+      session[:user_id] = @user.id
+      redirect_to account_path
+    else
+      # @failed_login = true
+      render :new
+    end
   
+  end
+
+  def destroy
+    session[:logged_in] = false
+    session.clear
+    redirect_to root_path
+  end
+
 end
