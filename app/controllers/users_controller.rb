db4f1af43ef0e5df98703c6d0ad96aaa0b6a8a62
Apprentice <apprentice@dbc-chicago-001.local>
2013-10-30 16:54:19 -0500

    simple sign_up form in layouts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,17 @@
 class UsersController < ApplicationController
-#undo later
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+      if @user.save
+        flash[:notice] = "Signup Successful!"
+      else
+        flash[:notice] = "Invalid signup"
+      end
+      render 'new'
+  end
+   
 end
