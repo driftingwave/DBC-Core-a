2c6ca33ccc6a285236cf3b73b5453e1dee2dcd6b
Ryan S. Kelley <alexanderlarson@gmail.com>
2013-10-31 17:16:51 -0500

    fixed syntax in tests, added routing tests

--- spec/controllers/users_rspec.rb
+++ spec/controllers/users_rspec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+# require_relative 'factories/users'
 
 describe UsersController do 
 	describe "GET #index" do
@@ -12,7 +13,12 @@ describe UsersController do
 		get :index
 		response.should render_template :index
 	end
+
+	it "should recognize our sweet custom routes" do
+		profile_path(5).should == '/profile/5'
+	end
 end
+######
 
 describe "GET #show" do
 	it "assigns the requested user to @user" do
@@ -20,6 +26,8 @@ describe "GET #show" do
 		get :show, id: user
 		assigns(:user).should eq(user)
 	end
+end
+######
 
 describe "POST create" do
 	context "with valid attributes" do
@@ -35,5 +43,7 @@ describe "POST create" do
 			expect{
 				post :create, user: Factory.attributes_for(:bad_user)
 				}.to change(User,:count).by(1)
-			}
+		end
+	end
 end
+
