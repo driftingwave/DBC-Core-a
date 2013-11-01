1fa2365da63d92ca5dc59d46696bf6dbc1a882d4
Elizabeth Engelman <elizabethengelman@Elizabeths-MacBook-Pro.local>
2013-11-01 15:31:38 -0500

    added a create action in the questions controller, and a new question form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,9 +2,17 @@ class QuestionsController < ApplicationController
 
   def new
     #where the magic happens
+    @question = Question.new
     @user = User.find(session[:user_id])
-    @topics = User.topics
+    @topics = User.all
   end
 
-
+  def create
+    @question = Question.new(body: params[:question][:body], user_id: session[:user_id])
+    if @question.save
+    	redirect_to question_path(@question)
+    else
+      redirect_to new_question_path
+    end
+  end
 end
