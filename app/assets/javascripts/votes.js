2a2e914a49eaf819ecde3db87ea6decc55a03898
Apprentice <apprentice@dbc-chicago-001.local>
2013-11-01 20:41:41 -0500

    WIP ajax vote up and down buttons; added logic for down vote vs up vote

--- app/assets/javascripts/votes.js
+++ app/assets/javascripts/votes.js
@@ -11,13 +11,20 @@ $(document).ready(function(){
 
     if ($('this').text("up"))
     {
-      var data = {answer_id: answerId, type: "up"}
+      var data = {answer_id: answerId, type: 1}
       var url = '/questions/up/'
-      
+
       $.post(url, data, function(response){
-
-
+        
+        
       })
+    }
+
+    else 
+    {
+      var dataDown = {answer_id: answerId, type: -1}
+      $.post(url, dataDown,)
     }
   })
 })
+
