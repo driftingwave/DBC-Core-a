1929da78373bd6198c16bcd461eb23e84805afc2
Ryan Kelley <rskelley9@gmail.com>
2013-11-03 17:50:11 -0600

    added answer form to answers#show page

---
+++ app/controllers/answers_controller.rb
@@ -0,0 +1,15 @@
+class AnswerController < ApplicationController
+
+  def create
+
+# Answer(id: integer, question_id: integer, body: text, user_id: integer,
+#   vote_total: integer, created_at: datetime, updated_at: datetime)
+
+    @user
+    @question
+    @answer
+
+  end
+
+
+end
